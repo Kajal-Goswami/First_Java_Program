8436cab32e02360bdfd2f7d132414290810c0ac6
Kajal Goswami <91484902+Kajal-Goswami@users.noreply.github.com>
2026-02-19 13:38:59 +0530

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,11 +1,12 @@
-public public class Testing {
+public class Testing {
 
     public int sum(int a, int b){
         return a + b;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Testing t = new Testing();
-        System.out.println("Sum = " + t.sum(10,5));
+        System.out.println(t.sum(10,5));
     }
 }
+
